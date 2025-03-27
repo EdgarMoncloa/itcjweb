04f9743635ddaf7a4476513ceb46a96aa68d002e
Edgar Moncloa <edgarmoncloa@gmail.com>
2025-03-27 10:26:54 -0600

    feat(templates): add storybook template

---
+++ src/tokens/StorybookTemplate.tsx
@@ -0,0 +1,21 @@
+import { StoryObj, Meta } from "@storybook/react";
+
+export default {
+  title: "Atoms/Grids/TransitionFadeGrid",
+  component: null,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+  args: {},
+  argTypes: {},
+};
+
+// type MyStoryProps = {};
+// type Story = StoryObj<MyStoryProps>;
+
+// export const Base: Story = {
+//   render: ({ ...args }) => {
+//     return null;
+//   },
+// };
