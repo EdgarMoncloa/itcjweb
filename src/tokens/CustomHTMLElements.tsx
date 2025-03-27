a75aa4c510ef636813081a027465bb925a580041
Edgar Moncloa <edgarmoncloa@gmail.com>
2025-03-27 12:43:13 -0600

    feat(StyledUl): improve UI

--- src/tokens/CustomHTMLElements.tsx
+++ src/tokens/CustomHTMLElements.tsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import Liebre_Icon from "/icons/Liebre_Icon.png";
 
 export const StyledHr = styled.hr`
   border: 0;
@@ -10,4 +11,43 @@ export const StyledBlockquote = styled.blockquote`
   background-color: var(--colors-app-primary-50);
   padding: var(--size-padding-xs) var(--size-padding-medium);
   border-radius: var(--size-border-radius-small);
+`;
+
+const cssRtl = css`
+  & li {
+    padding-right: var(--size-padding-medium);
+  }
+  & li::before {
+    right: 0;
+  }
+`;
+const cssLtr = css`
+  & li {
+    padding-left: var(--size-padding-medium);
+  }
+  & li::before {
+    left: 0;
+  }
+`;
+type StyledUlProps = {
+  $rtl?: boolean;
+};
+export const StyledUl = styled.ul<StyledUlProps>`
+  padding-inline-start: 0;
+  & li {
+    list-style-type: none;
+    position: relative;
+  }
+  & li::before {
+    content: "";
+    position: absolute;
+    top: var(--size-padding-small);
+    width: var(--font-size-body3);
+    height: var(--font-size-body3);
+    background-image: url(${Liebre_Icon});
+    background-size: var(--font-size-body3) var(--font-size-body3);
+  }
+
+  ${(p) => p.$rtl && cssRtl}
+  ${(p) => !p.$rtl && cssLtr}
 `;
